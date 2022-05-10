417787244ea71d1436ca2d6f5bb98512c6e7527b
Marina Andrade Catão <marinavdac@gmail.com>
2022-05-10 20:25:50 -0300

    req: adiciona requisito 02 do projeto

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        <h2>test</h2>
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
